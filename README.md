c73697204a520c23ee1bf8f6f9e2e9b90a0965ea
yourfhighness <k.joshua855@gmail.com>
2021-03-30 10:48:23 +0200

    Completed the calculator assignment

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# CcHUB-Challenge
-This is simple calculator challenge
+# CALCULATOR CHALLEGE ASSESSMENT
+
+- This Calculator Challenge
+#### This is the Hosted link of the calculator challenge [Access Calculator Direct]
+
+https://key-joshua.github.io/CcHUB-Challenge/html/index.html
+
+#### This is the Github repository link of the calculator challenge 
+
+https://github.com/key-joshua/CcHUB-Challenge
+
+
+## GETTING START WITH PROJECT
+
+- should clone this repository with ```git clone https://github.com/key-joshua/CcHUB-Challenge.git```
+- should switch into this branch with ```git checkout develop```
+
+- After diving into a branch, navigate inside html directory and run ```index.html``` file in any browser.
---
+++ javascript/calculator.js
@@ -0,0 +1,113 @@
+
+const input = document.getElementById('input'); // input/output button
+const number = document.querySelectorAll('.numbers div'); // number buttons
+const operator = document.querySelectorAll('.operators div'); // operator buttons
+const result = document.getElementById('result'); // equal button
+const clear = document.getElementById('clear'); // clear button
+let resultDisplayed = false; // flag to keep an eye on what output is displayed
+
+// adding click handlers to number buttons
+for (let i = 0; i < number.length; i += 1) {
+  number[i].addEventListener('click', (e) => {
+    if(input.innerHTML === '0.00') {
+      input.innerHTML = '';
+    }
+
+    // storing current input string and its last character in variables - used later
+    const currentString = input.innerHTML;
+    const lastChar = currentString[currentString.length - 1];
+
+    // if result is not diplayed, just keep adding
+    if (resultDisplayed === false) {
+      input.innerHTML += e.target.innerHTML;
+    } else if (resultDisplayed === true && lastChar === '+' || lastChar === '-' || lastChar === '×' || lastChar === '÷') {
+      // if result is currently displayed and user pressed an operator
+      // we need to keep on adding to the string for next operation
+      resultDisplayed = false;
+      input.innerHTML += e.target.innerHTML;
+    } else {
+      // if result is currently displayed and user pressed a number
+      // we need clear the input string and add the new input to start the new opration
+      resultDisplayed = false;
+      input.innerHTML = '0.00';
+      input.innerHTML += e.target.innerHTML;
+    }
+  });
+}
+
+// adding click handlers to number buttons
+for (let i = 0; i < operator.length; i += 1) {
+  operator[i].addEventListener('click', (e) => {
+    // storing current input string and its last character in variables - used later
+    const currentString = input.innerHTML;
+    const lastChar = currentString[currentString.length - 1];
+
+    // if last character entered is an operator, replace it with the currently pressed one
+    if (lastChar === '+' || lastChar === '-' || lastChar === '×' || lastChar === '÷') {
+      const newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
+      input.innerHTML = newString;
+    } else if (currentString.length == 0) {
+      // if first key pressed is an opearator, don't do anything
+      console.log('enter a number first');
+    } else {
+      // else just add the operator pressed to the input
+      input.innerHTML += e.target.innerHTML;
+    }
+  });
+}
+
+// on click of 'equal' button
+result.addEventListener('click', () => {
+  // this is the string that we will be processing eg. -10+26+33-56*34/23
+  const inputString = input.innerHTML;
+
+  // forming an array of numbers. eg for above string it will be: numbers = ['10', '26', '33', '56', '34', '23']
+  const numbers = inputString.split(/\+|\-|\×|\÷/g);
+
+  // forming an array of operators. for above string it will be: operators = ['+', '+', '-', '*', '/']
+  // first we replace all the numbers and dot with empty string and then split
+  const operators = inputString.replace(/[0-9]|\./g, '').split('');
+
+  // now we are looping through the array and doing one operation at a time.
+  // first divide, then multiply, then subtraction and then addition
+  // as we move we are alterning the original numbers and operators array
+  // the final element remaining in the array will be the output
+
+  let divide = operators.indexOf('÷');
+  while (divide !== -1) {
+    numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
+    operators.splice(divide, 1);
+    divide = operators.indexOf('÷');
+  }
+
+  let multiply = operators.indexOf('×');
+  while (multiply !== -1) {
+    numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+    operators.splice(multiply, 1);
+    multiply = operators.indexOf('×');
+  }
+
+  let subtract = operators.indexOf('-');
+  while (subtract !== -1) {
+    numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
+    operators.splice(subtract, 1);
+    subtract = operators.indexOf('-');
+  }
+
+  let add = operators.indexOf('+');
+  while (add !== -1) {
+    // using parseFloat is necessary, otherwise it will result in string concatenation :)
+    numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
+    operators.splice(add, 1);
+    add = operators.indexOf('+');
+  }
+
+  input.innerHTML = numbers[0]; // displaying the output
+
+  resultDisplayed = true; // turning flag if result is displayed
+});
+
+// clearing the input on press of clear
+clear.addEventListener('click', () => {
+  input.innerHTML = '0.00';
+});
